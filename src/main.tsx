099eccf0125f09ace0a55daff3b3eaf5ded1c5d2
nin-webapp2025 <niniwebapp@protonmail.com>
2026-07-08 14:32:52 +0100

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -6,5 +7,6 @@ import { ThemeProvider } from "@/components/theme-provider";
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="light" storageKey="nin-portal-theme">
     <App />
+    <Analytics />
   </ThemeProvider>
 );
