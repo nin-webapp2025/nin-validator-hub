3f315dd69a06003d54a81eddd2afcb538af74cb3
nin-webapp2025 <niniwebapp@protonmail.com>
2026-07-08 14:49:28 +0100

    Add Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.tsx";
 import "./index.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -8,5 +9,6 @@ createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="light" storageKey="nin-portal-theme">
     <App />
     <Analytics />
+    <SpeedInsights />
   </ThemeProvider>
 );
